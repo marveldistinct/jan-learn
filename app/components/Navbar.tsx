adab34f82f78c853e91c93f89070a61aea03f0d0
MarvelDistinct <macbook@Marvel-Mac.local>
2025-01-23 03:22:19 -0800

    yc_learn

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image';
+import{auth, signOut, signIn} from "@/auth"
 
-const Navbar = () => {
+
+const Navbar = async () => {
+  const session = await auth()
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'> 
     <nav className='flex justify-between items-center'>
@@ -10,7 +13,31 @@ const Navbar = () => {
 <Image src="/logo.png" alt="logo" width={144} height={30} />
 
         </Link>
+      <div className='flex items-center gap-5'>
+      {session && session ?. user? (
+          <>
+            <Link href='/startup/create'>
+               <span>Create</span>
+            </Link>
 
+            <button onClick={signOut}>
+              <span>Logout</span>
+
+            </button>
+
+            <Link href={`/user/${session?.id}`}>
+              <span>{session?.user?.name}</span>
+            </Link>
+          </>
+        ) : (
+          <button onClick={signIn( provider: 'github')}>
+              <span>Login</span>
+          </button>
+
+        )}
+
+
+      </div>
     </nav>
     </header>
   )
